144e68690135b8d17043b97b293aad8453611311
Logue <logue@hotmail.co.jp>
2012-03-06 23:40:37 +0900

    first commit

---
+++ ini.js
@@ -0,0 +1,238 @@
+/********************************************************************
+INI file class for JavaScript
+
+Copyright (c)2009-2012 Logue <http://logue.be/> All rights reserved.
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions 
+are met:
+
+* Redistributions of source code must retain the above copyright 
+notice, this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright 
+notice, this list of conditions and the following disclaimer in the 
+documentation and/or other materials provided with the distribution.
+
+* Neither the name of the nor the names of its contributors may be 
+used to endorse or promote products derived from this software 
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+// プロトタイプ
+Ini.prototype = {
+	meta: {
+		"@prefix": "<http://purl.org/net/ns/doas#>",
+		"@about": "<io.js>", a: ":JavaScript",
+		 title: "Ini file class",
+		 created: "2009-01-17", release: {revision: "1.2.0", created: "2012-03-06"},
+		 author: {name: "Logue", homepage: "<http://logue.be/>"},
+		 acknowledgement: {name: "shoji", 'homepage': "<http://shoji.blog1.fc2.com/blog-entry-130.html>" },
+		 license: "<http://www.gnu.org/licenses/gpl-3.0.html>"
+	},
+	initialize: function(file,isUnicode) {
+		this.clear();
+		this.isUnicode = (isUnicode) ? true : false;
+		if(window.ActiveXObject){
+			this.fso = new ActiveXObject("Scripting.FileSystemObject"); // FileSystemObjectを作成
+			file = file.replace('file:///','').replace(/\//g,'\\').replace(/%20/g,' ');	// FSOはURL形式を理解できない
+		}else if (window.Components){
+			netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
+			this.LocalFile  = Components.classes['@mozilla.org/file/local;1'].createInstance(Components.interfaces.nsILocalFile);
+			this.fileStream = Components.classes['@mozilla.org/network/file-input-stream;1'].createInstance(Components.interfaces.nsIFileInputStream);
+			this.ioService  = Components.classes['@mozilla.org/network/io-service;1'].getService(Components.interfaces.nsIIOService);
+			this.converterStream = Components.classes['@mozilla.org/intl/converter-input-stream;1'].createInstance(Components.interfaces.nsIConverterInputStream);
+		}else if(window.runtime.flash.filesystem ){
+			this.air = window.runtime.flash.filesystem;
+		}else{
+			alert("Ini: Not supported browser.");
+		}
+		this.open(file);
+	},
+
+	// openメソッド - Iniファイルの読込
+	open: function(filename) {
+		this.filename = (filename) ? filename : this.filename;
+		var stream;
+		if (this.fso){
+			if (this.fso.FileExists(filename)){
+				stream = this.fso.OpenTextFile(filename, 1, false, this.isUnicode).ReadAll(); // ファイルを開く
+				ini = stream.split('\n');
+			}else{
+				alert("Ini: '" + filename + "' is not found.");
+			}
+		}else if (window.Components){
+			var charset;
+			if (this.isUnicode){
+				charset = 'utf-16';	// Unicode
+			}else{
+				charset = 'Shift_JIS';	// ANSI
+			}
+			this.LocalFile.initWithPath(filename);
+			if (this.LocalFile.exists()){
+				this.fileStream.init(this.LocalFile, 1, 0, false);
+				this.converterStream.init(this.fileStream, charset, this.fileStream.available(),this.converterStream.DEFAULT_REPLACEMENT_CHARACTER);
+				var out = {};
+				this.converterStream.readString(this.fileStream.available(), out);
+				this.converterStream.close();
+				this.fileStream.close();
+				ini = out.value.split("\n");
+			}else{
+				alert("Ini: '" + filename + "' is not found.");
+			}
+		}else if(this.air){
+			if( this.air.File.filename.exists){
+				var filemode, charset;
+				// FSOのエミュレート
+				stream = new this.air.FileStream();
+				if (this.isUnicode){
+					charset = 'utf-16';	// Unicode
+				}else{
+					charset = this.air.File.systemCharset;	// ANSI
+				}
+				stream.open(filename, this.air.FileMode.READ);
+				stream.readMultiByte(filename.size, encording);
+				ini = stream.split(air.File.lineEnding);
+				stream.close();
+			}else{
+				alert("Ini: '" + filename + "' is not found.");
+			}
+		}
+		this.clear();
+		
+		var section, keyname, value;
+	
+		// 正規表現マッチパターン（SectionとKeyとValueにマッチ。Sectionとの判定は、4番目に=が含まれてるかで判定）
+		var pattern = /^(\[(.*?)\]|(.*?)\s*(=)\s*(.*?))[\.\s]*?$/;
+
+		for(var i = 0; i < ini.length; i++ ){
+			if (ini[i] && ini[i] != '\n'){
+				var line = ini[i].replace(/^[\s]+/,""); // 先頭の空白は削除
+				// 空行ではない
+				if( !line.match(/^\s*$/) && !line.match(/^[;\*-\/\/]/) ){
+					var args = line.match(pattern);
+					if (args){
+						args.splice(0,2);
+						if (!args[2]){
+							section = args[0];
+							this.items[ args[0] ] = []; // セクション行を追加
+						}
+						if(  args[2] === '=' && section !== null ) {
+							this.items[ section ][ args[1] ] = args[3];
+						}
+					}
+				}
+			}
+		}
+
+		this.filename = filename;
+		ini = null; delete ini;
+	},
+
+	// updateメソッド - iniファイルの更新
+	// 改良の余地あり。
+	update: function(filename) {
+		filename = ( filename == null ) ? this.filename : filename;
+		var ini;
+
+		try {
+			if (this.fso){
+				if (this.fso.FileExists(filename)){
+					ini = this.fso.OpenTextFile(this.filename,2,true,this.isUnicode)
+					for( var sectionname in this.items ){
+						ini.WriteLine( '[' + sectionname + ']' );
+						for( var keyname in this.items[sectionname] )
+							ini.WriteLine( keyname + '\t=\t' + this.items[sectionname][keyname] );
+						ini.WriteLine( '' );
+					}
+					ini.close();
+				}else{
+					alert("Ini: '" + filename + "' is not found.");
+				}
+			}else if(this.air){
+				var filemode, encording;
+				// FSOのエミュレート
+				ini = new this.air.FileStream();
+				if (this.isUnicode){
+					encording = 'utf-16';
+				}else{
+					encording = this.air.File.systemCharset;
+				}
+				if( this.air.File.filename.exists){
+					ini.open(filename, this.air.FileMode.WRITE);
+					ini.writeMultiByte(filename.size, encording);
+					for( var sectionname in this.items ){
+						ini.writeMultiByte( '[' + sectionname + ']' );
+						for( var keyname in this.items[sectionname] )
+							ini.writeMultiByte( keyname + '\t=\t' + this.items[sectionname][keyname] );
+						ini.writeMultiByte( '' );
+					}
+					ini.close();
+				}else{
+					alert("Ini: '" + filename + "' is not found.");
+				}
+			}else if(window.Components){
+				if (filename.exists()) {
+					var ini = Components.classes['@mozilla.org/network/file-output-stream;1'].createInstance(Components.interfaces.nsIFileOutputStream);
+					ini.init(filename, 2, 0x200, false); // open as "write only"
+					for( var sectionname in this.items ){
+						ini.push( '[' + sectionname + ']' );
+						for( var keyname in this.items[sectionname] ) ini.push(keyname + '\t=\t' + this.items[sectionname][keyname] );
+						ini.push( '' );
+					}
+
+					var out = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance (Components.interfaces.nsIFileOutputStream);
+					out.init (filename, 0x20 | 0x02, 00004, null);
+					out.write(tbox.value, tbox.textLength);
+					out.close();
+				}else{
+					alert("Ini: '" + filename + "' is not found.");
+				}
+			}
+			ini = null; delete ini;
+		}catch(e){
+			console.error(e);
+		}
+	},
+
+	// clearメソッド - 全削除
+	clear: function() {
+		try { delete this.items; } catch(e) {}
+		this.items = new Array(); // 項目
+		this.filename = null; // ファイル名
+	},
+
+	// getItemメソッド - 項目の値読み込み
+	getItem: function(sectionname, keyname) {
+		if( sectionname in this.items )
+		if( keyname in this.items[sectionname] )
+		return this.items[sectionname][keyname];
+	},
+
+	// setItemメソッド - 項目の値設定
+	setItem: function(sectionname, keyname, value, updateflag) {
+		if( updateflag == null ) updateflag = true;
+		if( !(sectionname in this.items) )
+		this.items[ sectionname ] = new Array();
+		this.items[ sectionname ][ keyname ] = value;
+		if( updateflag && this.filename!=null ) this.update();
+	}
+};
+
+// 各コンストラクタ
+function Ini(){
+	this.initialize.apply(this,arguments);
+}
---
+++ io.js
@@ -0,0 +1,490 @@
+// Html Aplication IO Script
+// Copyright (c)2009 Logue <http://logue.be/> All rights reserved.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Usage :
+//   var io = new Io();
+//   filecontent = io.read(filename);
+
+// Notice:
+//   This script is use for local aplication(HTA, air ,widgit etc).
+//   DO NOT USE INTERNET APLICATION, for security reson.
+
+// Constractor
+function Io(){
+	this.initialize.apply(this,arguments);
+}
+
+// prototype
+Io.prototype = {
+	meta: {
+		"@prefix": "<http://purl.org/net/ns/doas#>",
+		"@about": "<io.js>", a: ":JavaScript",
+		 title: "Html Aplication IO Script",
+		 created: "2009-01-17", release: {revision: "1.0.1", created: "2009-04-21"},
+		 author: {name: "Logue", homepage: "<http://logue.be/>"},
+		 license: "<http://www.gnu.org/licenses/gpl-3.0.html>"
+	},
+	initialize: function() {
+		// ブラウザ判別
+		this.isIE = window.ActiveXObject;
+		this.isFF = window.Components;
+
+		// オブジェクト生成
+		if (this.isIE){
+			this.wsh = new ActiveXObject( "WScript.Shell" );
+			this.wsc = new ActiveXObject( "WSHController" );
+			this.fso = new ActiveXObject( "Scripting.FileSystemObject" );
+			this.sct = new ActiveXObject( "ScriptControl" );
+			this.sha = new ActiveXObject( "Shell.Application" );
+			this.xml = new ActiveXObject( "Microsoft.XMLDOM" );
+
+			if (!this.user) this.user = "USER";	// 操作する環境変数の特権クラス
+		}else if (this.isFF){
+			netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
+		}else if (window.runtime.flash.filesystem){
+			// Adobe Air用（未テスト）
+			this.air = window.runtime.flash.filesystem;
+		}else if (widget.system){
+			// MacOS X dashbord用（未テスト）
+			this.sh = widget.system;
+			if (window.WFile) console.error('WFile is not installed. please referer to http://www.adamrocker.com/blog/160/wfile_reference.html');
+		}else{
+			console.error('This script works only ActiveX or AIR or Dashboard supported browser.');
+		}
+	},
+	// レジストリ読み込み（Windowsのみ）
+	readReg : function(key) {
+		if (this.wsh){
+			try{ 
+				return this.wsh.RegRead(key);
+			}catch(e){
+				return false;
+			}
+		}else{
+			return false;
+		}
+	},
+	//レジストリ書き込み（Windowsのみ）
+	writeReg : function (key, value, type){
+		if (this.wsh){
+			try{ 
+				return this.wsh.RegWrite(key,value,type);
+			}catch(e){
+				return e;
+			}
+		}else{
+			return false;
+		}
+	},
+	//レジストリ削除（Windowsのみ）
+	deleReg : function (key){
+		if (this.wsh){
+			try{ 
+				return this.wsh.RegDelete(key);
+			}catch(e){
+				return e;
+			}
+		}else{
+			return false;
+		}
+	},
+	//レジストリサブキー一覧（Windowsのみ）
+	listReg : function (key){
+		if (this.sct){
+			var regs = key.split('\\');
+			var nRegRoot;
+			var sRegPath = regs.shift().join('\\');
+			switch (regs[0]){
+				case 'HKCR': case 'HKEY_CLASSES_ROOT' :
+					nRegRoot = '0x80000000';
+				break;
+				case 'HKCU': case 'HKEY_CURRENT_USER' :
+					nRegRoot = '0x80000001';
+				break;
+				case 'HKLM': case 'HKEY_LOCAL_MACHINE' :
+					nRegRoot = '0x80000002';
+				break;
+				case 'HKU' : case 'HKEY_USERS' :
+					nRegRoot = '0x80000003';
+				break;
+				case 'HKEY_CURRENT_CONFIG' :
+					nRegRoot = '0x80000004';
+				break;
+			}
+			// VBSなんて知らないよ
+			var sRegProv = "winmgmts:{impersonationLevel=impersonate}!\\\\.\\root\\default:StdRegProv";
+			var sScriptText = [
+				'Function RegEnum(sRegProv, nRegRoot, sRegPath)',
+				'	Call GetObject(sRegProv).EnumKey(nRegRoot, sRegPath, RegEnum2)',
+				'End Function'
+			].join("\n");
+
+			this.sct.Language = "VBScript";
+			this.sct.AddCode(sScriptText);
+			try{
+				var sResult = this.sct.Run("RegEnum", sRegProv, nRegRoot, sRegPath);
+				if(sResult == null) return null;
+				return new VBArray(sResult).toArray();
+			}catch(e){
+				return e;
+			}
+		}else{
+			return false;
+		}
+	},
+	// 環境変数取得（Windows／Macのみ）
+	readEnv : function (env){
+		if (this.wsh){
+			return this.wsh.Environment(this.user).Item(env);
+		}else if (this.sh){
+			return this.sh('printenv '+ env,null).outputString;
+		}else{
+			return false;
+		}
+	},
+	// 環境変数設定（Windows／Macのみ）
+	setEnv : function (env,value){
+		if (this.wsh){
+			this.wsh.Environment(this.user).Item(env) = value;
+			return true;
+		}else if (this.sh){
+			return this.sh('export '+ env + ' ' + value ,null).outputString;
+		}else{
+			return false;
+		}
+	},
+	// 環境変数削除（Windows／Macのみ）
+	deleEnv : function (env){
+		if (this.wsh){
+			this.wsh.Environment(this.user).Remove(env);
+			return true;
+		}else if (this.sh){
+			return this.sh('unset '+ env,null).outputString;
+		}else{
+			return false;
+		}
+	},
+	// ファイル存在確認
+	exsists : function(name){
+		if (this.fso){
+			if (this.fso.FileExists(name)){
+				return true;
+//			}else if (name.match(/\\$/) && this.fso.FolderExists(name)){
+//				return 'folder';
+			}else{
+				return false;
+			}
+		}else if(this.sh){
+			if (this.sh(name + ' -f',null).outputString){
+				return true;
+			}else if (this.sh(name + ' -d',null).outputString){
+				return 'folder';
+			}else{
+				return false;
+			}
+		}else if(this.air){
+			return this.air.File.name.exists;
+		}
+	},
+	// コマンド実行（Windows／Macのみ）
+	// コールバック関数が入っている場合は、非同期通信になる
+	exec : function(cmd, async) {
+		if (this.exsists(file) == true){
+			var oExec = this.wsh.Exec(cmd);	// 非同期実行を開始してWshExecオブジェクトを得る
+			if (this.wsh){
+				(function commandWaitLoop() {
+					switch(oExec.Status) {
+						// WshRunning - 未だ実行中
+						case 0: 
+							// 100ミリ秒後にもう一度statusを見ることにする
+							setTimeout(commandWaitLoop, 100);
+							if (async) async();
+						break;
+
+						// WshFinished - コマンドの実行が完了した
+						case 1:
+							// 標準出力の内容を全て読み取ってコールバック関数に渡す
+							return process.StdOut.ReadAll();
+						break;
+
+						// WshFailed - コマンドの実行に失敗した
+						case 2: 
+							// 例外を投げる
+							throw new Error("asyncExecuteCommand failed");
+					}
+				})();
+			}else if(this.sh){
+				return widget.system(cmd, null).outputString;
+			}else{
+				return null;
+			}
+		}else{
+			return false;
+		}
+	},
+	// 実行（Windows／Macのみ）
+	run : function(file){
+		if (this.wsh){
+			try{
+				this.wsh.Run("\""+file+"\"",0,false);
+			}catch(e){
+				// UACなどでキャンセルした場合エラーになるため、エラー消し
+//				console.error(e);
+			}
+		}else if(this.sh){
+			try{
+				widget.openApplication(file);	// com.aplication.itunesみたいな形式で入力。たぶん使わないだろうなぁ。
+			}catch(e){
+				this.sh(file,null);
+			}
+		}
+	},
+	echo : function(str){
+		if (WScript){
+			WScript.Echo(str);
+		}else{
+			alert(str);
+		}
+	},
+	// バージョン（Windowsのみ）
+	version : function(file) {
+		if (this.fso && this.exsists(file) != false){
+			try{
+				return this.fso.GetFileVersion(file);
+			}catch(e){
+				return e;
+			}
+		}else{
+			return false;
+		}
+	},
+	// ファイル読み込み（Macの場合、WFileプラグインが必要）
+	read : function(file, type){
+		if (this.exsists(file) == true){
+			if (this.fso){
+				try {
+					var content = this.fso.OpenTextFile(file, 1).ReadAll();
+					return content;
+					content.close();
+				}catch (e){
+					return e;
+				}
+			} else if(navigator.vendorSub){
+				try {
+					var file = Components.classes["@mozilla.org/file/local;1"].createInstance (Components.interfaces.nsILocalFile);
+					file.initWithPath (filePath);
+					var fstream = Components.classes["@mozilla.org/network/file-input-stream;1"].createInstance(Components.interfaces.nsIFileInputStream);
+					fstream.init(file, -1, 0, 0);
+					var charset = "UTF-8";
+//					const replacementChar = Components.interfaces.nsIConverterInputStream.DEFAULT_REPLACEMENT_CHARACTER;
+					var is = Components.classes["@mozilla.org/intl/converter-input-stream;1"].createInstance(Components.interfaces.nsIConverterInputStream);
+					is.init(fstream, charset, 1024, replacementChar);
+					var str = {};
+					while (is.readString(4096, str) != 0) {
+						data += str.value;
+					}
+				} catch (e) {
+					return e;
+				}
+				return data;
+			}else if(window.WFile){
+				return WFile.read(file);
+			}else if(this.air){
+				var data = new this.air.FileStream();
+				data.open(file, this.air.FileMode.READ);
+				return data.readMultiByte(file.size, this.air.File.systemCharset);
+				data.close();
+			}
+		}else{
+			return false;
+		}
+	},
+	// ファイル保存
+	save : function(file, stream){
+		if (this.fso){
+			try {
+				var data = this.fso.OpenTextFile(file, 2, true);
+				data.Write(stream);
+				data.Close();
+				return true;
+			}catch (e){
+				return e;
+			}
+		}else if(navigator.vendorSub){
+			try {
+				var file = Components.classes["@mozilla.org/file/local;1"].createInstance (Components.interfaces.nsILocalFile);
+				file.initWithPath (file);
+				if (! file.exists ()) file.create (0, 0664);
+				var out = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance (Components.interfaces.nsIFileOutputStream);
+				out.init (file, 0x20 | 0x02, 00004, null);
+				var charset = "UTF-8";
+				var conv = Components.classes["\@mozilla.org/intl/converter-output-stream;1"].createInstance(Components.interfaces.nsIConverterOutputStream);
+				conv.init(out, charset, 0, 0x0000);
+				conv.writeString(stream);
+				conv.close();
+				return true;
+			}catch (e){
+				return e;
+			}
+		}else if (window.WFile){
+			WFile.write(file, stream);
+			return true;
+		}else if (this.air){
+			var data = new this.air.FileStream();
+			data.open(file, this.air.FileMode.WRITE);
+			data.writeUTFBytes(stream);
+			data.close();
+			return true;
+		}
+		return false;
+	},
+	// ファイル書き込み
+	wrire : function(file, stream ,line){
+		
+	},
+	// 新規ファイル／ディレクトリ(fileが、\か/で終わる場合はディレクトリとする)
+	create: function(file,data){
+		if (file.match(/\\$/) || file.match(/\/$/)){
+			if (this.fso){
+				this.fso.CreateFolder(file);
+			}else if(window.WFile){
+				WFile.mkdir(file);
+			}else if (this.air){
+				this.air.File.createDirectory()
+			}
+		}else{
+			if(this.fso){
+				var data = this.fso.CreateTextFile(file, true);
+				data.writeLine(data);
+				data.Close();
+			}else if(window.WFile){
+				
+			}else if (this.air){
+				
+			}
+		}
+	},
+	// ファイル／ディレクトリを削除
+	dele : function(file){
+		if (this.exsists(file) != false){
+			if (this.fso){
+				if (file.match(/\\$/)){
+					this.fso.DeleteFolder(file);
+				}else{
+					this.fso.DeleteFile(file);
+				}
+				return true;
+			}else if(this.sh){
+				if (file.match(/\/$/)){
+					this.sh('rm -r '+file,null);
+				}else{
+					this.sh('rm '+file,null);
+				}
+				return true;
+			}else if(this.air){
+				this.air.File.moveToTrash(file);
+				return true;
+			}
+		}
+		return false;
+	},
+	// ファイル／ディレクトリを移動
+	move  : function(from,to){
+		if (this.fso){
+			if (from.match(/\\$/)){
+				this.fso.MoveFolder(from,to);
+			}else{
+				this.fso.MoveFile(from,to);
+			}
+		}else if(this.sh){
+			this.sh('mv '+from+' '+to,null);
+		}else if(this.air){
+			var sourceFile = this.air.File.documentsDirectory;
+			sourceFile = sourceFile.resolvePath(from);
+			var destination = this.air.File.documentsDirectory;
+			destination = destination.resolvePath(to);
+			sourceFile.moveTo(destination, true);
+		}
+	},
+	// ファイル／ディレクトリのコピー
+	copy  : function(from,to){
+		if (this.fso){
+			if (from.match(/\\$/)){
+				this.fso.CopyFolder(from,to);
+			}else{
+				this.fso.CopyFile(from,to);
+			}
+		}else if(this.sh){
+			this.sh('cp '+from+' '+to,null);
+		}else if(this.air){
+			var sourceFile = air.File.documentsDirectory;
+			sourceFile = sourceFile.resolvePath(from);
+			var destination = air.File.documentsDirectory;
+			destination = destination.resolvePath(to);
+			sourceFile.copyTo(destination, true);
+		}
+	},
+	// ディレクトリ一覧（Windowsの場合のみ\で終わった場合）
+	list : function(name){
+		if (this.exsists(name) == true){
+			if(this.fso){
+				if (name.match(/\\$/)){
+					return this.fso.GetFolder(name);
+				}else{
+					return this.fso.GetFile(name);
+				}
+			}else if(this.sh){
+				return this.sh('ls '+name,null).outputString;
+			}else if(this.air){
+				return this.air.File.name.getDirectoryListing();
+			}
+		}else{
+			return false;
+		}
+	},
+	bin2hex : function(str){
+		// http://phpjs.org/functions/bin2hex:48e4b527-20c0-487d-b0ef-2d6486a786ee
+		var v,i, f = 0, a = [];
+		s += '';
+		f = s.length;
+
+		for (i = 0; i<f; i++) {
+			a[i] = s.charCodeAt(i).toString(16).replace(/^([\da-f])$/,"0$1");
+		}
+		return a.join('');
+	},
+	// Zip圧縮（Windowsのみ）
+	zip : function (file, source){
+		var sBaseFolder = this.fso.GetParentFolderName(WScript.ScriptFullName);
+		this.wsh.CurrentDirectory = sBaseFolder;
+
+		sDestFile = this.fso.BuildPath(sBaseFolder, sDestFile);
+		sSourcePath = this.fso.BuildPath(sBaseFolder, sSourcePath);
+
+		if(!this.fso.FolderExists(source)) return false;
+
+		var sZipHead = "PK\05\06";
+		sZipHead += "\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00";
+
+		var oZipFile = this.fso.CreateTextFile(file, true);
+		oZipFile.Write(sZipHead);
+		oZipFile.Close();
+
+		var oZipFolder = this.sha.NameSpace(sDestFile);
+		oZipFolder.CopyHere(sSourcePath);
+		return true;
+	}
+};
